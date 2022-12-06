7b3f2794a19df73838d1711ed15320133bb0ee9d
phenomenal62777 <58382760+phenomenal62777@users.noreply.github.com>
2022-12-06 00:37:16 -0800

    image target

---
+++ ar foundation/Assets/imagetrack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class imagetrack : MonoBehaviour
+{
+    private ARTrackedImageManager _aRTrackedImageManager;
+
+    private void Awake()
+    {
+        _aRTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+    public void OnEnable()
+    {
+        _aRTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+    public void OnDisable()
+    {
+        _aRTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+
+    }
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach(var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+        }
+    }
+
+
+}
